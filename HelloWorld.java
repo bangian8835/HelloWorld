611525e7f8f9741ee695a7bc30cffd6dbae8ac23
Nick Bangia <bangian8835@my.uwstout.edu>
2024-09-08 08:47:57 -0500

    Added a Nintendo DS reference

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("\n/!\\ WARNING - HEALTH AND SAFETY");
+        System.out.println("BEFORE PLAYING, READ THE HEALTH");
+        System.out.println("AND SAFETY PRECAUTIONS BOOKLET");
+        System.out.println("FOR IMPORTANT INFORMATION");
+        System.out.println("ABOUT YOUR HEALTH AND SAFETY");
+        System.out.println("\nTo get an extra copy for your region, go online at");
+        System.out.println("www.nintendo.com/healthsafety/");
+        System.out.println("\nPress any button to continue.\n");
     }
 }
